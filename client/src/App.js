8b366d0081ddc7921c25393be6a038703c2219ce
piyaarora <arorapiya141@gmail.com>
2021-05-21 17:40:27 +0530

    frontend added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import DetailState from './context/detail/DetailState'
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
+import setAuthToken from './utils/setAuthToken'
+import PrivateRoute from './components/routing/PrivateRoute';
+import FormCont from './components/authentication/FormCont';
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthState>
+      <DetailState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path='/' component={Home} />
+                  <Route exact path='/register' component={FormCont} />
+                  <Route exact path='/login' component={FormCont} />
+
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </DetailState>
+    </AuthState>
   );
 }
 
